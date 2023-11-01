3b05496d97882c186976dfda28001750c238c7c1
Vivek Sunil Jadhav <vivek.jadhav.edu@gmail.com>
2023-11-02 00:53:45 +0530

    modefird services section

---
+++ varadvinayak_website/app/api/ServicesApi.js
@@ -0,0 +1,32 @@
+const ServicesApi = [
+    {
+        // id: 1,
+        image: "https://images.pexels.com/photos/5998120/pexels-photo-5998120.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title: 'Ready to move Sites',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Qui Lorem ipsum dolor sit amet consectetur adipisicing elit.fioh asfihgsdfgi sdh giosidfo sihg siohg0ayef adshfo aohdfo0-e-f uoasdihfoihas df',
+        url: '/services/readysites'
+    },
+    {
+        // id: 2,
+        image: "https://images.pexels.com/photos/18821289/pexels-photo-18821289/free-photo-of-building-with-apartments.jpeg",
+        title: 'Cummercial Sites',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Qui Lorem ipsum dolor sit amet consectetur adipisicing elit.fioh asfihgsdfgi sdh giosidfo sihg siohg0ayef adshfo aohdfo0-e-f uoasdihfoihas df',
+        url: '/services/cummercial'
+    },
+    {
+        // id: 3,
+        image: "https://images.pexels.com/photos/10804653/pexels-photo-10804653.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title: 'Agricultural lands',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Qui Lorem ipsum dolor sit amet consectetur adipisicing elit.fioh asfihgsdfgi sdh giosidfo sihg siohg0ayef adshfo aohdfo0-e-f uoasdihfoihas df',
+        url: '/services/agricultural'
+    },
+    {
+        // id: 4,
+        image: "https://images.pexels.com/photos/46160/field-clouds-sky-earth-46160.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title: 'Open Land Projects',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Qui Lorem ipsum dolor sit amet consectetur adipisicing elit.fioh asfihgsdfgi sdh giosidfo sihg siohg0ayef adshfo aohdfo0-e-f uoasdihfoihas df',
+        url: '/services/openlands'
+    }
+];
+
+export default ServicesApi;
